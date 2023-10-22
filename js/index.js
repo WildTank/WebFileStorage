f74db8279552675a5a6df48cc13ac4edf4b4ee8c
WildTank <tantanuchikori@gmail.com>
2023-10-22 17:45:09 +0800

    basic working dropdown menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("Connected!");
+
+const dropdownMenus = document.querySelectorAll(".dropdown-menu")
+dropdownMenus.forEach(dropdown => {
+    dropdown.classList.toggle("hide")
+})
+
+const dropdownButton = document.querySelectorAll(".dropdown-button")
+dropdownButton.forEach(button => {
+    button.addEventListener("click", () => {
+        button.nextElementSibling.classList.toggle("hide")
+    })
+})
