5588c590ff4f339aa99abeb1c6b5b2e25c62dec8
Wild-Tanuki <tantanuchikori@gmail.com>
2023-10-23 12:18:48 +0800

    improved dropdown functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 console.log("Connected!");
 
-const dropdownMenus = document.querySelectorAll(".dropdown-menu")
-dropdownMenus.forEach(dropdown => {
-    dropdown.classList.toggle("hide")
-})
-
-const dropdownButton = document.querySelectorAll(".dropdown-button")
+const dropdownButton = document.querySelectorAll(".dropdown-button");
 dropdownButton.forEach(button => {
     button.addEventListener("click", () => {
-        button.nextElementSibling.classList.toggle("hide")
+        button.nextElementSibling.classList.toggle("hide");
     })
+})
+
+const dropdownMenus = document.querySelectorAll(".dropdown-menu");
+document.addEventListener("click", (e) => {
+    if (e.target.closest(".dropdown-button") === null) {
+        for (let i = 0; i < dropdownMenus.length; i++) {
+            if (dropdownMenus[i].classList.contains("hide") === false) {
+                dropdownMenus[i].classList.toggle("hide");
+            }
+        }
+    } else {
+        let currentDropdown = e.target.parentElement.nextElementSibling;
+        for (let i = 0; i < dropdownMenus.length; i++) {
+            if (dropdownMenus[i] !== currentDropdown) {
+                if (dropdownMenus[i].classList.contains("hide") === false) {
+                    dropdownMenus[i].classList.toggle("hide");
+                }
+            }
+        }
+    }
 })
