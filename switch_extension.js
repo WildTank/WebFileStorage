8dd77493d9416500464b66a68c0c82b71c88687f
WildTank <tantanuchikori@gmail.com>
2023-11-16 21:27:31 +0800

    Finished switch extension js script

--- switch_extension.js
+++ switch_extension.js
@@ -3,7 +3,7 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 const htmlPaths = ["./index.html", "./settings.html", "./codes-archive.html", "./logs-archive.html",
 "./modules-archive.html", "./news-archive.html", "./single-post.html"];
-const phpPath = ["./index.php", "./settings.php", "./codes-archive.php", "./logs-archive.php",
+const phpPaths = ["./index.php", "./settings.php", "./codes-archive.php", "./logs-archive.php",
 "./modules-archive.php", "./news-archive.php", "./single-post.php"];
 
 
@@ -12,12 +12,12 @@ const convertTo = prompt("Convert to 'PHP' or 'HTML': ");
 try {
   if (convertTo == "PHP") {
     for (i = 0; i < htmlPaths.length; i++) {
-      fs.renameSync(htmlPaths[i], phpPath[i]);
+      fs.renameSync(htmlPaths[i], phpPaths[i]);
       console.log(`${i+1} Switch successful!`);
     }
   } else if (convertTo == "HTML") {
-    for (i = 0; i < phpPath.length; i++) {
-      fs.renameSync(phpPath[i], htmlPaths[i]);
+    for (i = 0; i < phpPaths.length; i++) {
+      fs.renameSync(phpPaths[i], htmlPaths[i]);
       console.log(`${i+1} Switch successful!`);
     }
   } else {
@@ -29,3 +29,19 @@ try {
   return 1;
 }
 
+// replaces href links of html code
+if (convertTo == "PHP") {
+  const regex = /\.html/g;
+  for (i = 0; i < htmlPaths.length; i++) {
+    let codeFileString = fs.readFileSync(phpPaths[i]).toString();
+    let commitReplace = codeFileString.replace(regex, ".php");
+    fs.writeFileSync(phpPaths[i], commitReplace);
+  }
+} else if (convertTo == "HTML") {
+  const regex = /\.php/g;
+  for (i = 0; i < htmlPaths.length; i++) {
+    let codeFileString = fs.readFileSync(htmlPaths[i]).toString();
+    let commitReplace = codeFileString.replace(regex, ".html");
+    fs.writeFileSync(htmlPaths[i], commitReplace);
+  }
+}
